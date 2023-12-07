e5bcc6c965047828d460afb3cbcfb9037abedea1
Justin L. Brown <brownjl@cpchem.com>
2023-12-06 20:03:35 -0600

    i'll come back for this one

--- day05/script.py
+++ day05/script.py
@@ -29,19 +29,31 @@ class ThingToThingMap:
                 return mapping['destination'] + (source - mapping['source'])
         # Return the source number when no covering mapping is found.
         return source
+    def get_source(self, destination : int) -> int:
+        for mapping in self.mappings:
+            # Isolate a mapping that covers the desitnation number.
+            if (mapping['destination'] <= destination) and (mapping['destination'] + mapping['range'] > destination):
+                return mapping['source'] + (destination - mapping['destination'])
+        # Return the destination number when no covering mapping is found.
+        return destination
 
 # Define the puzzle solver.
 class Solver:
+    mapping_names = None
+
     def __init__(self, input = []):
         self.input = input
 
     def parse_input(self, input : "list(str)" = None):
+        self.mapping_names = []
         result = {}
         for line in input or self.input:
             if re.match('seeds:', line):
                 result['seeds'] = [int(x) for x in re.findall(r'(\d+)', line)]
             elif re.match(r'([a-z-]+) map:', line):
-                current_mapping = result[line[:-5]] = ThingToThingMap()
+                mapping_name = line[:-5]
+                current_mapping = result[mapping_name] = ThingToThingMap()
+                self.mapping_names.append(mapping_name)
             elif re.match(r'(\d+) (\d+) (\d+)', line):
                 current_mapping.add_mapping(line)
         self.parsed_input = result
@@ -50,16 +62,15 @@ class Solver:
     def get_location_for_seed(self, seed : int, parsed_input : dict = None) -> int:
         parsed = parsed_input or self.parsed_input
         result = seed
-        for x in [
-            'seed-to-soil',
-            'soil-to-fertilizer',
-            'fertilizer-to-water',
-            'water-to-light',
-            'light-to-temperature',
-            'temperature-to-humidity',
-            'humidity-to-location'
-        ]:
+        for x in self.mapping_names:
             result = parsed[x].get_destination(result)
+        return result
+    
+    def get_seed_for_location(self, location : int, parsed_input : dict = None) -> int:
+        parsed = parsed_input or self.parsed_input
+        result = location
+        for x in [x for x in reversed(self.mapping_names)]:
+            result = parsed[x].get_source(result)
         return result
 
     def part_1(self):
@@ -67,10 +78,40 @@ class Solver:
         print(f"Part 1: {result}")
 
     def part_2(self):
-        result = sys.maxsize
-        for start, length in zip(self.parsed_input['seeds'][0::2],self.parsed_input['seeds'][1::2]):
-            for seed in range(start,length):
-                result = min(result, self.get_location_for_seed(seed))
+        seeds = list(zip(self.parsed_input['seeds'][0::2],self.parsed_input['seeds'][1::2]))
+        humidity_to_location_mapping = self.parsed_input['humidity-to-location'].mappings.copy()
+        humidity_to_location_mapping.sort(key=lambda x: x['destination'])
+        result = None
+        # ---
+        # Attempt 2: Walk through mappings to bracketize splits.
+        # ---
+        sources = [x[0] for x in seeds]
+        destinations = []
+        for mapping_name in self.mapping_names:
+            for source in sources:
+                destination = self.parsed_input[mapping_name].get_destination(source)
+                destinations.append(destination)
+            sources = destinations.copy()
+            destinations = []
+        sources.sort()
+        logging.debug(sources)
+        result = self.get_seed_for_location(sources[0])
+        
+        # ---
+        # Attempt 1: Brute force through locations to find the first that maps back to a seed.
+        # ---
+        # for humidity_to_location in humidity_to_location_mapping:
+        #     for x in range(humidity_to_location['destination'],humidity_to_location['destination'] + humidity_to_location['range']):
+        #         seed = self.get_seed_for_location(x)
+        #         for seed_bucket in seeds:
+        #             if (seed_bucket[0] <= seed) and (seed_bucket[0] + seed_bucket[1] > seed):
+        #                 logging.debug(f"Seed for location {x} is {seed} in seed range ({seed_bucket[0],seed_bucket[1]}).")
+        #                 result = seed
+        #             break
+        #         if result is not None:
+        #             break
+        #     if result is not None:
+        #         break
         print(f"Part 2: {result}")
 
 # Parse the input file.
